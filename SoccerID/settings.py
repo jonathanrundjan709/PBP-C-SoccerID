b4c94fa6114d74f74b84ae15f7f1cb23e92d453e
Jonathan Yitskhaq Rundjan <jonathanrundjan@Ishaks-Laptop.local>
2025-09-08 15:58:14 +0700

    setting

--- SoccerID/settings.py
+++ SoccerID/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jonathan-yitskhaq-SoccerID.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jonathan-yitskhaq-soccerid.pbp.cs.ui.ac.id"]
 
 
 
