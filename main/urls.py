4f42a572315b8556ef8962d5a0308f50f2410f58
Jonathan Yitskhaq Rundjan <jonathanrundjan@Ishaks-Laptop.local>
2025-10-01 10:38:26 +0700

    Tugas 5 Nihh

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,18 @@
 # main/urls.py
-from django.urls import path
+from django.urls import path, re_path
 from main.views import (
-    show_main, add_product, show_product,
-    show_xml, show_json, show_xml_by_id, show_json_by_id,
-    register, login_user, logout_user,
-    edit_product, delete_product,
+    show_main,
+    add_product,
+    show_product,
+    show_xml,
+    show_json,
+    show_xml_by_id,
+    show_json_by_id,
+    register,
+    login_user,
+    logout_user,
+    edit_product,
+    delete_product,
 )
 
 app_name = 'main'
@@ -12,14 +20,32 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-product/', add_product, name='add_product'),
-    path('product/<uuid:id>/', show_product, name='show_product'),
+
+    # ==== TERIMA UUID ATAU INT ====
+    re_path(
+        r'^product/(?P<id>(?:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}|\d+))/$',
+        show_product,
+        name='show_product'
+    ),
+    re_path(
+        r'^product/(?P<id>(?:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}|\d+))/edit/$',
+        edit_product,
+        name='edit_product'
+    ),
+    re_path(
+        r'^product/(?P<id>(?:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}|\d+))/delete/$',
+        delete_product,
+        name='delete_product'
+    ),
+    # ==== ===================== ====
+
+    # API (biarkan seperti semula jika memang ingin UUID saja)
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
+
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('product/<uuid:id>/edit/', edit_product, name='edit_product'),
-    path('product/<uuid:id>/delete/', delete_product, name='delete_product'),
 ]
