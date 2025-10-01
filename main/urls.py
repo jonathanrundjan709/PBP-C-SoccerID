01b13b7f83a715dc62eab3201c4309344072833f
Jonathan Yitskhaq Rundjan <jonathanrundjan@Ishaks-Laptop.local>
2025-10-01 10:31:36 +0700

    Tugas 5 Ni

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,10 @@
+# main/urls.py
 from django.urls import path
 from main.views import (
-    show_main,
-    add_product,
-    show_product,
-    show_xml,
-    show_json,
-    show_xml_by_id,
-    show_json_by_id,
-    register,
-    login_user,
-    logout_user,
-    edit_product,
-    delete_product,
-    
+    show_main, add_product, show_product,
+    show_xml, show_json, show_xml_by_id, show_json_by_id,
+    register, login_user, logout_user,
+    edit_product, delete_product,
 )
 
 app_name = 'main'
@@ -21,15 +13,13 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-product/', add_product, name='add_product'),
     path('product/<uuid:id>/', show_product, name='show_product'),
-    path('xml/', show_xml, name='show_xml'),        
-    path('json/', show_json, name='show_json'),          
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
     path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('product/<uuid:id>/edit', edit_product, name='edit_product'),
-    path('news/<uuid:id>/delete', delete_product, name='delete_product'),
-
-    
+    path('product/<uuid:id>/edit/', edit_product, name='edit_product'),
+    path('product/<uuid:id>/delete/', delete_product, name='delete_product'),
 ]
